66fc091a3e84796f387a93312dec2d34d285256c
TJ Saunders <tj@castaglia.org>
2021-05-17 18:23:46 -0700

    Issue #1: Implement support for handling SSH passwords.

--- t/lib/ProFTPD/Tests/Modules/mod_passwd_update.pm
+++ t/lib/ProFTPD/Tests/Modules/mod_passwd_update.pm
@@ -6,6 +6,7 @@ use strict;
 
 use Carp;
 use Cwd;
+use File::Copy;
 use File::Path qw(mkpath);
 use File::Spec;
 use IO::Handle;
@@ -43,13 +44,58 @@ my $TESTS = {
     test_class => [qw(forking)],
   },
 
+  passwd_update_sftp_ignore_publickey_auth => {
+    order => ++$order,
+    test_class => [qw(forking mod_sftp)],
+  },
+
+  passwd_update_sftp_handle_password_auth => {
+    order => ++$order,
+    test_class => [qw(forking mod_sftp)],
+  },
 };
 
 sub new {
   return shift()->SUPER::new(@_);
 }
 
+sub set_up {
+  my $self = shift;
+  $self->SUPER::set_up(@_);
+
+  # Make sure that mod_sftp does not complain about permissions on the hostkey
+  # files.
+
+  my $rsa_host_key = File::Spec->rel2abs('t/etc/modules/mod_passwd_update/ssh_host_rsa_key');
+  unless (chmod(0400, $rsa_host_key)) {
+    die("Can't set perms on $rsa_host_key: $!");
+  }
+}
+
 sub list_tests {
+  # Check for the required Perl modules:
+  #
+  #  Net-SSH2
+  #  Net-SSH2-SFTP
+
+  my $required = [qw(
+    Net::SSH2
+    Net::SSH2::SFTP
+  )];
+
+  foreach my $req (@$required) {
+    eval "use $req";
+    if ($@) {
+      print STDERR "\nWARNING:\n + Module '$req' not found, skipping all tests\n";
+
+      if ($ENV{TEST_VERBOSE}) {
+        print STDERR "Unable to load $req: $@\n";
+      }
+
+      return qw(testsuite_empty_test);
+    }
+  }
+
   return testsuite_get_runnable_tests($TESTS);
 }
 
@@ -674,6 +720,312 @@ sub passwd_update_algo_des {
 
       $self->assert($ok,
         test_msg("Did not see expected 'DES salt' log messages"));
+
+    } else {
+      die("Can't read $setup->{log_file}: $!");
+    }
+  };
+  if ($@) {
+    $ex = $@;
+  }
+
+  test_cleanup($setup->{log_file}, $ex);
+}
+
+sub passwd_update_sftp_ignore_publickey_auth {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+  my $setup = test_setup($tmpdir, 'passwd_update');
+
+  my $new_auth_user_file = $setup->{auth_user_file};
+  $new_auth_user_file .= '.new';
+
+  my $rsa_host_key = File::Spec->rel2abs('t/etc/modules/mod_passwd_update/ssh_host_rsa_key');
+
+  my $rsa_priv_key = File::Spec->rel2abs('t/etc/modules/mod_passwd_update/test_rsa_key');
+  my $rsa_pub_key = File::Spec->rel2abs('t/etc/modules/mod_passwd_update/test_rsa_key.pub');
+  my $rsa_rfc4716_key = File::Spec->rel2abs('t/etc/modules/mod_passwd_update/authorized_rsa_keys');
+
+  my $authorized_keys = File::Spec->rel2abs("$tmpdir/.authorized_keys");
+  unless (copy($rsa_rfc4716_key, $authorized_keys)) {
+    die("Can't copy $rsa_rfc4716_key to $authorized_keys: $!");
+  }
+
+  my $config = {
+    PidFile => $setup->{pid_file},
+    ScoreboardFile => $setup->{scoreboard_file},
+    SystemLog => $setup->{log_file},
+    TraceLog => $setup->{log_file},
+    Trace => 'passwd_update:20 passwd_update.file:20 passwd_update.lock:20 passwd_update.passwd:20 passwd_update.salt:20',
+
+    AuthUserFile => $setup->{auth_user_file},
+    AuthGroupFile => $setup->{auth_group_file},
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+
+      'mod_passwd_update.c' => {
+        PasswordUpdateEngine => 'on',
+        PasswordUpdateLog => $setup->{log_file},
+        PasswordUpdateAuthUserFiles => "$setup->{auth_user_file} $new_auth_user_file",
+      },
+
+      'mod_sftp.c' => {
+        SFTPEngine => 'on',
+        SFTPLog => $setup->{log_file},
+        SFTPHostKey => $rsa_host_key,
+        SFTPAuthorizedUserKeys => 'file:~/.authorized_keys',
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($setup->{config_file},
+    $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  require Net::SSH2;
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      sleep(1);
+
+      my $ssh2 = Net::SSH2->new();
+      unless ($ssh2->connect('127.0.0.1', $port)) {
+        my ($err_code, $err_name, $err_str) = $ssh2->error();
+        die("Can't connect to SSH2 server: [$err_name] ($err_code) $err_str");
+      }
+
+      unless ($ssh2->auth_publickey($setup->{user}, $rsa_pub_key,
+          $rsa_priv_key)) {
+        my ($err_code, $err_name, $err_str) = $ssh2->error();
+        die("RSA publickey authentication failed: [$err_name] ($err_code) $err_str");
+      }
+
+      $ssh2->disconnect();
+    };
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($setup->{config_file}, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($setup->{pid_file});
+  $self->assert_child_ok($pid);
+
+  eval {
+    if (open(my $fh, "< $setup->{log_file}")) {
+      my $ok = 0;
+
+      while (my $line = <$fh>) {
+        chomp($line);
+
+        if ($ENV{TEST_VERBOSE}) {
+          print STDERR "# $line\n";
+        }
+
+        if ($line =~ /skipping password migration for ssh2 protocol session with publickey authentication/) {
+          $ok = 1;
+          last;
+        }
+      }
+
+      close($fh);
+
+      $self->assert($ok,
+        test_msg("Did not see expected 'no entry found' log messages"));
+
+    } else {
+      die("Can't read $setup->{log_file}: $!");
+    }
+  };
+  if ($@) {
+    $ex = $@;
+  }
+
+  test_cleanup($setup->{log_file}, $ex);
+}
+
+sub passwd_update_sftp_handle_password_auth {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+  my $setup = test_setup($tmpdir, 'passwd_update');
+
+  my $new_auth_user_file = $setup->{auth_user_file};
+  $new_auth_user_file .= '.new';
+
+  my $algos = 'sha512 sha256';
+  if ($^O eq 'darwin') {
+    # Mac's crypt(3) doesn't support SHA256/SHA512.  Sigh.
+    $algos = 'des';
+  }
+
+  my $rsa_host_key = File::Spec->rel2abs('t/etc/modules/mod_passwd_update/ssh_host_rsa_key');
+
+  my $rsa_priv_key = File::Spec->rel2abs('t/etc/modules/mod_passwd_update/test_rsa_key');
+  my $rsa_pub_key = File::Spec->rel2abs('t/etc/modules/mod_passwd_update/test_rsa_key.pub');
+  my $rsa_rfc4716_key = File::Spec->rel2abs('t/etc/modules/mod_passwd_update/authorized_rsa_keys');
+
+  my $authorized_keys = File::Spec->rel2abs("$tmpdir/.authorized_keys");
+  unless (copy($rsa_rfc4716_key, $authorized_keys)) {
+    die("Can't copy $rsa_rfc4716_key to $authorized_keys: $!");
+  }
+
+  my $config = {
+    PidFile => $setup->{pid_file},
+    ScoreboardFile => $setup->{scoreboard_file},
+    SystemLog => $setup->{log_file},
+    TraceLog => $setup->{log_file},
+    Trace => 'passwd_update:20 passwd_update.file:20 passwd_update.lock:20 passwd_update.passwd:20 passwd_update.salt:20',
+
+    AuthGroupFile => $setup->{auth_group_file},
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+
+      'mod_passwd_update.c' => {
+        PasswordUpdateEngine => 'on',
+        PasswordUpdateLog => $setup->{log_file},
+        PasswordUpdateAuthUserFiles => "$setup->{auth_user_file} $new_auth_user_file",
+        PasswordUpdateAlgorithms => $algos,
+      },
+
+      'mod_sftp.c' => {
+        SFTPEngine => 'on',
+        SFTPLog => $setup->{log_file},
+        SFTPHostKey => $rsa_host_key,
+        SFTPAuthorizedUserKeys => 'file:~/.authorized_keys',
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($setup->{config_file},
+    $config);
+
+  # Order of directives matters here, thus why we add these lines last.
+  if (open(my $fh, ">> $setup->{config_file}")) {
+    print $fh "AuthUserFile $new_auth_user_file\n";
+    unless (close($fh)) {
+      die("Can't write $setup->{config_file}: $!");
+    }
+
+  } else {
+    die("Can't open $setup->{config_file}: $!");
+  }
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  require Net::SSH2;
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      sleep(1);
+
+      my $ssh2 = Net::SSH2->new();
+      unless ($ssh2->connect('127.0.0.1', $port)) {
+        my ($err_code, $err_name, $err_str) = $ssh2->error();
+        die("Can't connect to SSH2 server: [$err_name] ($err_code) $err_str");
+      }
+
+      unless ($ssh2->auth_password($setup->{user}, $setup->{passwd})) {
+        my ($err_code, $err_name, $err_str) = $ssh2->error();
+        die("Password authentication failed: [$err_name] ($err_code) $err_str");
+      }
+
+      $ssh2->disconnect();
+
+      # Now login again
+      $ssh2 = Net::SSH2->new();
+      unless ($ssh2->connect('127.0.0.1', $port)) {
+        my ($err_code, $err_name, $err_str) = $ssh2->error();
+        die("Can't connect to SSH2 server: [$err_name] ($err_code) $err_str");
+      }
+
+      unless ($ssh2->auth_password($setup->{user}, $setup->{passwd})) {
+        my ($err_code, $err_name, $err_str) = $ssh2->error();
+        die("Password authentication failed: [$err_name] ($err_code) $err_str");
+      }
+
+      $ssh2->disconnect();
+    };
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($setup->{config_file}, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($setup->{pid_file});
+  $self->assert_child_ok($pid);
+
+  eval {
+    if (open(my $fh, "< $setup->{log_file}")) {
+      my $ok = 0;
+
+      while (my $line = <$fh>) {
+        chomp($line);
+
+        if ($ENV{TEST_VERBOSE}) {
+          print STDERR "# $line\n";
+        }
+
+        if ($line =~ /successfully updated password hash for user '$setup->{user}'/) {
+          $ok = 1;
+          last;
+        }
+      }
+
+      close($fh);
+
+      $self->assert($ok,
+        test_msg("Did not see expected 'no entry found' log messages"));
 
     } else {
       die("Can't read $setup->{log_file}: $!");
